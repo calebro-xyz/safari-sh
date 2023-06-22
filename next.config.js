36f095d0ad860345a5bc1c3ba8c908692f37fbe9
calebro <ckmuithi@gmail.com>
2023-06-22 23:55:04 +0300

    go

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["tailwindui.com", "images.unsplash.com", "images.pexels.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/ui/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/stories/**/*.{js,ts,jsx,tsx,mdx}",
+    "./mail/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  plugins: [
+    require("@tailwindcss/aspect-ratio", "@tailwindcss/forms", "@tailwindcss/line-clamp", "@tailwindcss/typography"),
   ],
   theme: {
+    colors: {
+      background: "#fffffe",
+      primary: "#078080",
+      secondary: "#f45d48",
+      tertary: "#f8f5f2",
+      heading: "#232323",
+      content: "#222525",
+    },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
+};
